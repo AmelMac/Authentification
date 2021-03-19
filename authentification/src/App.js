7b8c8f9c51dff912b33025bb5c5a5cac24ad7fbc
root <root@PC0988-SI.santelys.fr>
2021-03-19 17:03:10 +0100

    add french to react admin

--- authentification/src/App.js
+++ authentification/src/App.js
@@ -8,12 +8,18 @@ import { UserList } from './users';
 import Dashboard from './Dashboard';
 import AuthProvider from './authProvider';
 import { PostList, PostEdit, PostCreate } from './posts';
+import frenchMessages from 'ra-language-french';
+import polyglotI18nProvider from 'ra-i18n-polyglot';
 
 import './App.css';
+const messages = {
+  'fr': frenchMessages,
+};
+const i18nProvider = polyglotI18nProvider(locale => messages[locale]);
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dashboard={Dashboard} authProvider={AuthProvider} dataProvider={dataProvider}>
+  <Admin locale="fr" i18nProvider={i18nProvider} dashboard={Dashboard} authProvider={AuthProvider} dataProvider={dataProvider}>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
